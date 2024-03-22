7156bc0a9b2b2a684429b23238cf0e3e129dbd22
admin <admin@DESKTOP-TF1K6QG>
2024-03-22 10:53:08 +0530

    Commit for update in framework

---
+++ DemoProjectGitHub/src/test/java/organization_Anjali/ToPracticePullAfterUpdate.java
@@ -0,0 +1,10 @@
+package organization_Anjali;
+
+public class ToPracticePullAfterUpdate {
+
+	public static void main(String[] args) {
+		System.out.println("Check for update in framework as ATE");
+
+	}
+
+}
